8d127f2b1d9323512ac83dc711c98cebd380f9d1
jjyoon7 <jay.yoon7@gmail.com>
2020-01-30 18:19:55 +0100

    'useEffect' to countdown the times.

--- speed-type-game/src/App.js
+++ speed-type-game/src/App.js
@@ -1,15 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function App() {
   const [text, setText] = useState("");
-  // const [time, setTime] = useState(0);
+  const [time, setTime] = useState(10);
 
   function handleChange(event) {
     const {value} = event.target;
     setText(value)
   };
 
+  function calculateWordCount(words) {
+    const wordsArr = words.trim().split(" ");
+    const wordsCount = wordsArr.filter(word => word !== "").length;
+    return wordsCount;
+  }
 
+  useEffect(() => {
+    let timer;
+    if(time > 0) {
+      let timer = setTimeout(() => {
+        setTime(time => time - 1)
+      }, 1000)
+    } else {
+      return () => clearTimeout(timer)
+    }
+  }, [time])
   // function setTime() {
   //   setInterval(() => {
   //     time - 1;
@@ -24,9 +39,9 @@ function App() {
     <main>
       <h1>speed type game</h1>
       <textarea onChange={handleChange} value={text}/>
-      {/* <h4>{time}</h4>
-      <button onClick={handleStart}>start</button>
-      <h1>word count {wordcount}</h1> */}
+      <h4>time remaining: {time}</h4>
+      <button onClick={() => console.log(calculateWordCount(text))}>start</button>
+      {/* <h1>word count {wordsCount}</h1> */}
     </main>
   )
 }
