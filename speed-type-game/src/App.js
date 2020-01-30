1cfe8b844f98010dc17c17da770e85259f5383ec
jjyoon7 <jay.yoon7@gmail.com>
2020-01-30 15:02:39 +0100

    prep pt 1

--- speed-type-game/src/App.js
+++ speed-type-game/src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import React, {userState} from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [time, setTime] = useState(0);
+
+  function setText() {
+    userState(prevText => ({
+      [value]: prevText
+    }))
+  };
+
+  function setTime() {
+    setInterval(() => {
+      time - 1;
+    }, 1000)
+  }
+
+  function handleStart() {
+    setTime()
+  }
+
   return (
-    <div className="App">
-    </div>
-  );
+    <main>
+      <h1>speed type game</h1>
+      <textarea value={text} onChange={setText}/>
+      <h4>{time}</h4>
+      <button onClick={handleStart}>start</button>
+    </main>
+  )
 }
 
-export default App;
+export default App
