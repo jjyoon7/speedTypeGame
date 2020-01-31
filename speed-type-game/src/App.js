4b7d5e8b883307ec9c5bc2e49d29a33f802c4087
jjyoon7 <jay.yoon7@gmail.com>
2020-01-31 20:55:05 +0100

    timing with useEffect()

--- speed-type-game/src/App.js
+++ speed-type-game/src/App.js
@@ -1,17 +1,43 @@
-import React from "react";
-import useWordGame from "./hooks/useWordGame"
+import React, {useState, useEffect} from "react"
 
 function App() {
-  const {inputRef, isTimeRunning, handleChange, text, time, startGame, finalCount} = useWordGame(5)
+  const [text, setText] = useState("")
+  const [time, setTime] = useState(5)
+  const [isTimerunning, setIsTimeRunning] = useState(false)
+
+  //what needs to happen
+  //calculate the words
+  //display the word count
+  //count down the time
+  //when coutdown is done, you cannnot type
+  //if the game did not start, you cannot type
+
+
+  function handleChange(event) {
+    const {value} = event.target
+    setText(value)
+  }
+
+  function startGame() {
+    //count down starts
+    //
+  }
+
+  useEffect(() => {
+    if(isTimerunning) {
+      setTime(prevTime => prevTime - 1)
+    } else if(isTimerunning === 0) {
+      setIsTimeRunning(false)
+    }
+  }, [isTimerunning])
 
   return (
-    <main>
+    <div>
       <h1>speed type game</h1>
-      <textarea ref={inputRef} disabled={!isTimeRunning} onChange={handleChange} value={text}/>
+      <textarea type="text" value={text} onChange={handleChange}/>
+      <button onClick={startGame}>start</button>
       <h4>time remaining: {time}</h4>
-      <button disabled={isTimeRunning} onClick={startGame}>start</button>
-      <h1>word count: {finalCount}</h1>
-    </main>
+    </div>
   )
 }
 
